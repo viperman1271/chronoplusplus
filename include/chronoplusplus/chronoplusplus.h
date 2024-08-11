d3825961eb3a91e081b4ca1deef9332d505ebd9f
Michael Filion <viperman1271@hotmail.com>
2024-08-11 11:32:22 -0400

    Properly supporting the outputting of the local_time

--- include/chronoplusplus/chronoplusplus.h
+++ include/chronoplusplus/chronoplusplus.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <string>
+#include <sstream>
 
 namespace chronoplusplus
 {
@@ -9,8 +13,17 @@ namespace chronoplusplus
     static std::string output_local_time(const std::chrono::system_clock::time_point time_point, const std::string& time_format)
     {
         std::time_t time_t_format = std::chrono::system_clock::to_time_t(time_point);
+#if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
+        std::tm tm_local;
+        localtime_s(&tm_local, &time_t_format);
+        std::tm* time_tm_format = &tm_local;
+#else
         std::tm* time_tm_format = std::localtime(&time_t_format);
-        return std::put_time(time_tm_format, time_format);
+#endif // _MSC_VER
+
+        std::stringstream ss;
+        ss << std::put_time(time_tm_format, time_format.c_str());
+        return ss.str();
     }
 
     static std::string output_local_time(const std::chrono::system_clock::time_point time_point)
